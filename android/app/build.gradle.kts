f4306fc2b8e87b1ee860901f3ab1abab88083f79
Helina Ariva <helina@nevercode.io>
2025-02-19 17:25:03 +0200

    Update build.gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 
     buildTypes {
         getByName("release") {
+            isShrinkResources = true
             isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("release")
         }
